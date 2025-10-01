ac95977d40feb9e1b0451e5444c0a08fc7d364ef
Ischim <marc.ischi@hes-so.ch>
2025-10-01 16:31:03 +0200

    Create Class Pixel

---
+++ src/main/java/ch/heigvd/commands/img/Pixel.java
@@ -0,0 +1,47 @@
+package ch.heigvd.commands.img;
+
+public class Pixel {
+    private byte blue;
+    private byte green;
+    private byte red;
+
+    public Pixel(byte blue, byte green, byte red) {
+        this.blue = blue;
+        this.green = green;
+        this.red = red;
+    }
+
+    public byte getRed() {
+        return red;
+    }
+
+    public byte getGreen() {
+        return green;
+    }
+
+    public byte getBlue() {
+        return blue;
+    }
+
+    public byte[] getPixel() {
+        return new byte[]{red, green, blue};
+    }
+
+    public void setRed(byte red) {
+        this.red = red;
+    }
+
+    public void setGreen(byte green) {
+        this.green = green;
+    }
+
+    public void setBlue(byte blue) {
+        this.blue = blue;
+    }
+
+    public void setPixel(byte blue, byte green, byte red) {
+        setBlue(blue);
+        setGreen(green);
+        setRed(red);
+    }
+}

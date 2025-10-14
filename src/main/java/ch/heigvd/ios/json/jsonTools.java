3d63e158633c9750f8c812f1efcbb563cbb7f355
Ischim <marc.ischi@hes-so.ch>
2025-10-14 21:30:10 +0200

    add comment in the code

--- src/main/java/ch/heigvd/ios/json/jsonTools.java
+++ src/main/java/ch/heigvd/ios/json/jsonTools.java
@@ -9,6 +9,7 @@ public class jsonTools {
     StringBuilder json = new StringBuilder();
     json.append("{\n");
 
+    // Creation of the json content
     for (int i = 0; i < coordinate.length; ++i) {
       String key = (i + 1) + (i == 0 ? "st" : i == 1 ? "nd" : i == 2 ? "rd" : "th") + " sus : ";
       json.append("\t\"")
@@ -25,6 +26,7 @@ public class jsonTools {
     }
     json.append("}");
 
+    // Write the json content to the file
     try (BufferedOutputStream bosJson =
         new BufferedOutputStream(new FileOutputStream(jsonFilename))) {
       bosJson.write(json.toString().getBytes());
